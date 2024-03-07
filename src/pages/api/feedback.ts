61145059d72d4db1061ad5fc3216b0872ac91837
Adon <a@atriskmedia.com>
2024-03-06 19:58:12 -0500

    update htmx example

--- src/pages/api/feedback.ts
+++ src/pages/api/feedback.ts
@@ -1,11 +1,11 @@
 import type { APIRoute } from "astro";
 
 export const POST: APIRoute = async ({ request }) => {
-  console.log(`running on server?`);
   const data = await request.formData();
   const name = data.get("name");
   const email = data.get("email");
   const message = data.get("message");
+  console.log(`running on server?`, name, email, message);
   // Validate the data - you'll probably want to do more than this
   if (!name || !email || !message) {
     return new Response(
