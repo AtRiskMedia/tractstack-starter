4e7e93448c048a1f8d80f83fc67350439feeff72
Adon <a@atriskmedia.com>
2024-11-10 23:30:18 -0500

    added README, LICENSE

--- scripts/prepare-template.sh
+++ scripts/prepare-template.sh
@@ -7,6 +7,7 @@ mkdir -p template
 
 # Copy core files
 cp -r playground/{src,public} template/
+cp playground/.prett* template/
 cp playground/.env.template template/.env
 cp playground/{astro.config.mjs,tailwind.config.cjs,tsconfig.json,env.d.ts,package.json} template/
 
