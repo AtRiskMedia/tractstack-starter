bc69b9984fc03500a726c6698faad97fae5108ef
Adon <a@atriskmedia.com>
2024-11-18 01:50:55 -0500

    proper package.json

--- scripts/prepare-template.sh
+++ scripts/prepare-template.sh
@@ -10,6 +10,7 @@ mkdir -p template
 
 # Copy core files
 cp -r playground/{src,public} template/
+cp playground/package.json template/
 cp playground/.prett* template/
 cp playground/.env.template template/.env
 cp playground/{astro.config.mjs,tailwind.config.cjs,tsconfig.json,env.d.ts} template/
