46d03391ed42775618dbc4e1cd354e361781cbf7
Adon <a@atriskmedia.com>
2024-11-22 19:51:50 -0500

    fixed identifyAs matching

--- playground/src/components/widgets/IdentifyAs.tsx
+++ playground/src/components/widgets/IdentifyAs.tsx
@@ -122,7 +122,7 @@ export const IdentifyAs = ({
           <SingleIdentifyAs
             key={`${value.slug}-${index}`}
             value={{ ...value, target }}
-            target={target}
+            target={target.toUpperCase()}
             readonly={readonly}
           />
         ))}
