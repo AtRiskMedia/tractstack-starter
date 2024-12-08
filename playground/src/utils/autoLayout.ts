a7bd0a09e8e6d0c6534ef5dc19fc788be8b0cc6a
Nazar Derevetskyy <nazmouse22@hotmail.com>
2024-12-08 02:27:45 -0500

    Fix overrides getting lost when changing layout

--- playground/src/utils/autoLayout.ts
+++ playground/src/utils/autoLayout.ts
@@ -2,25 +2,39 @@ import type { PaneDesign, PaneDesignMarkdown } from "@/types.ts";
 import { paneFragmentBgColour, paneFragmentIds, paneFragmentMarkdown } from "@/store/storykeep.ts";
 
 export const applyLayoutChange = (paneId: string, paneDesign: PaneDesign) => {
-  if(paneId?.length === 0 || !paneDesign) return;
+  if (paneId?.length === 0 || !paneDesign) return;
 
   const fragments = paneFragmentIds.get()[paneId].current;
   const concretePaneFragment = paneFragmentMarkdown.get()[fragments.last()];
-  if(!concretePaneFragment) return;
+  if (!concretePaneFragment) return;
 
   // overwrite class names and payloads
-  const firstFragment = paneDesign.fragments[0] as PaneDesignMarkdown;
-  const firstGragmentPayload = firstFragment.optionsPayload;
-  concretePaneFragment.current.payload.optionsPayload.classNamesPayload = {...firstGragmentPayload.classNamesPayload};
-  if(firstGragmentPayload.classNamesParent) {
-    concretePaneFragment.current.payload.optionsPayload.classNamesParent = { ...firstGragmentPayload.classNamesParent };
+  const newFragment = paneDesign.fragments[0] as PaneDesignMarkdown;
+  const newFragmentPayload = newFragment.optionsPayload;
+  const currentPaneFragmentPayload = concretePaneFragment.current.payload.optionsPayload;
+
+  // save original snapshot before the changes
+  const payloadSnapshot = { ...currentPaneFragmentPayload.classNamesPayload };
+  currentPaneFragmentPayload.classNamesPayload = { ...newFragmentPayload.classNamesPayload };
+  Object.keys(currentPaneFragmentPayload.classNamesPayload).forEach((className) => {
+    // if original snapshot had overrides then apply them to a new payload
+    if (payloadSnapshot[className].override) {
+      currentPaneFragmentPayload.classNamesPayload[className].override = {
+        ...payloadSnapshot[className].override,
+      };
+    }
+  });
+  // copy parent classes
+  if (newFragmentPayload.classNamesParent) {
+    currentPaneFragmentPayload.classNamesParent = { ...newFragmentPayload.classNamesParent };
   }
-  if(firstGragmentPayload.classNames) {
-    concretePaneFragment.current.payload.optionsPayload.classNames = { ...firstGragmentPayload.classNames };
+  // copy class names
+  if (newFragmentPayload.classNames) {
+    currentPaneFragmentPayload.classNames = { ...newFragmentPayload.classNames };
   }
 
   // update background
-  const paneBgs = {...paneFragmentBgColour.get()};
+  const paneBgs = { ...paneFragmentBgColour.get() };
   paneBgs[fragments[0]].current.bgColour = paneDesign.panePayload.bgColour as string;
   paneFragmentBgColour.set(paneBgs);
-}
+};
