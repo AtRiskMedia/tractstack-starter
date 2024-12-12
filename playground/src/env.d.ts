6d04572505cebc9cee0b2cb41cf9f9cd02dad4a8
Adon <a@atriskmedia.com>
2024-12-11 22:45:51 -0500

    tidy up env.d

--- playground/src/env.d.ts
+++ playground/src/env.d.ts
@@ -1,2 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../.astro/types.d.ts" />
+
+declare namespace App {
+  interface Locals {
+    user?: {
+      isAuthenticated: boolean;
+      isAdmin: boolean;
+      isOpenDemo: boolean;
+    };
+  }
+}
